f26f5291b7fd34801f3a533f84fbf8fe1a1e6e42
studentzainab <88240719+studentzainab@users.noreply.github.com>
2022-04-04 20:30:32 -0400

    Add files via upload

---
+++ p100/class.py
@@ -0,0 +1,22 @@
+class skyscraper(object):
+    #blueprint of the building
+    def __init__(self,model,colour,company,speed_limit):
+        self.matrial=matrial
+        self.company=company
+        self.size=size
+      
+
+    def start(self):
+        print("glass")
+
+    def stop(self):
+        print("GMR")
+
+    def accelarate(self):
+        print("247 meters")
+
+
+
+GMR=Car("Skyscraper","Glass","GMR","247M")
+
+print(skyscraper.stop())
